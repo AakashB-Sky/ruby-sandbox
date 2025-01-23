c93713915e55e7b8b30b35c0b14855063c659d9a
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-01-23 16:47:10 +0000

    updates

--- goodbye.rb
+++ goodbye.rb
@@ -1,2 +1,1 @@
-pp "Well, that was fun"
 pp "See ya later!"
--- hello.rb
+++ hello.rb
@@ -1,3 +1,4 @@
+# hello.rb
 require "active_support/all"
 require "awesome_print"
 require "pry-byebug"
@@ -5,6 +6,8 @@ require "pry-byebug"
 pp "Hi! Where are you located?"
 
 x = gets.chomp
-pp x
+pp "I see, you're in #{x}"
+
+require "./goodbye.rb" # this executes in the code goodbye.rb
 
 pp "Program finished"
